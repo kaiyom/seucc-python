411e9b8079b783322e57e478fd2f87b89632888a
Kaiyom Mohammad <49792282+kaiyom@users.noreply.github.com>
2021-10-31 18:08:44 +0600

    Create task_8.py

---
+++ task_8.py
@@ -0,0 +1,13 @@
+FILE = "mysquad.txt"
+team_name = "plus"
+team = ["Lionel Messi", "Cristiano Ronaldo", "Neymar", 
+        "Zlatan Ibrahimović", "Ronaldinho", "Robert Lewandowski"]
+msg = f"My favorite Football team is: {team_name}\n"
+counter = 1
+increment = 2
+
+with open(FILE, "w") as fileWritter:
+    fileWritter.write(msg)
+    for player in team[::-1]:
+        fileWritter.write(f"{counter} ==> {player}\n")
+        counter += increment
